f80149e6d24a351f78fcd487a5be48e5e1bec586
bradymiller <bradymiller@users.sourceforge.net>
2014-07-11 23:04:08 -0700

    yet more cleanup

--- demo_build.sh
+++ demo_build.sh
@@ -286,7 +286,7 @@ if $demoSSH; then
   unzip "$ds.zip"
   cd "$ds"
   #install openvpn
-  apt-get update
+  apt-get update >> $LOG
   apt-get -y install openvpn >> $LOG
   #initiate up ssh tunnel
   bash connect.sh >> $LOG
@@ -365,7 +365,7 @@ if $wordpressDemo; then
  mysql -u root wordpress < "$GITDEMOWORDPRESSDEMOSQL"
 
  # Install Postfix to allow email registration on wordpress patient portal demo
- apt-get update
+ apt-get update >> $LOG
  debconf-set-selections <<< "postfix postfix/mailname string 'demo.open-emr.org'"
  debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
  apt-get -y install postfix >> $LOG
