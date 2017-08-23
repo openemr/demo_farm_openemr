e26a317763a39918c425ce8dd0092964bb341ed7
Brady Miller <brady.g.miller@gmail.com>
2017-08-23 11:05:14 -0700

    Update restartFarm.sh

--- docker/scripts/restartFarm.sh
+++ docker/scripts/restartFarm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
+# Copyright (C) 2017 Brady Miller <brady.g.miller@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
 
 # stop and remove all containers
 docker stop $(docker ps -aq)
