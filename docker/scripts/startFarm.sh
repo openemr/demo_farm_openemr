0351d920e1d62d242ad3bdb042748860ac8b9fe9
Brady Miller <brady.g.miller@gmail.com>
2017-08-23 11:04:57 -0700

    Update startFarm.sh

--- docker/scripts/startFarm.sh
+++ docker/scripts/startFarm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
+# Copyright (C) 2017 Brady Miller <brady.g.miller@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
 
 # create aws ami t2.medium ec2 instance with 50GB storage space (can take up lots of storage space)
 #  -install docker and git via link: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/docker-basics.html
