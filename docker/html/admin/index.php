faf52a8fea58cfa9dc82fd004715a30f60ef83ee
Brady Miller <brady.g.miller@gmail.com>
2017-09-17 03:09:25 -0700

    beginning to work on admin page

--- docker/html/admin/index.php
+++ docker/html/admin/index.php
@@ -1,3 +1,24 @@
-TEST PAGE
+<?php
+/**
+ * OpenEMR Demo farm Admin Page
+ *
+ * @link      http://www.open-emr.org
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2017 Brady Miller <brady.g.miller@gmail.com>
+ * @license   GNU General Public License 3
+ */
+?>
 
-<?php echo phpinfo(); ?>
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="page-header">
+            <h1>OpenEMR Demo Farm Admin Page</h1>
+        </div>
+    </div>
+</body>
+
