f7955cc4db9bb79b83752dbdb53e2138ea419392
Brady Miller <brady.g.miller@gmail.com>
2017-10-01 17:39:22 -0700

    reorg demo farm website and add edu page

---
+++ docker/html/edu/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * OpenEMR Education, Research, and Instructional Projects Demos Page
+ *
+ * @link      http://www.open-emr.org
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2017 Brady Miller <brady.g.miller@gmail.com>
+ * @license   GNU General Public License 3
+ */
+?>
+
+<html>
+<head>
+
+    <meta name=viewport content="width=device-width, initial-scale=1">
+
+    <title>OpenEMR Education, Research, and Instructional Projects Demos Page</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="page-header">
+            <h1>OpenEMR Education, Research, and Instructional Projects Demos Page</h1>
+        </div>
+    </div>
+</div>
+</body>
+</html>
