c22ee6a60387a1fe574e9c18cf5db82e3d3fb17c
Brady Miller <brady.g.miller@gmail.com>
2018-11-30 01:21:13 -0800

    Update portal_onsite_and_wordpress.sql

--- pieces/portal_onsite_and_wordpress.sql
+++ pieces/portal_onsite_and_wordpress.sql
@@ -4,3 +4,4 @@ REPLACE INTO globals VALUES ('gbl_portal_cms_enable'  , 0, '1');
 REPLACE INTO globals VALUES ('gbl_portal_cms_address' , 0, 'http://demo.open-emr.org:2104/wordpress');
 REPLACE INTO globals VALUES ('gbl_portal_cms_username', 0, 'cmsportal');
 REPLACE INTO globals VALUES ('gbl_portal_cms_password', 0, 'cmsportal');
+REPLACE INTO globals VALUES ('rest_api', 0, '1');
