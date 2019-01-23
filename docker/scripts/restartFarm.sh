8046f7da70be73c3f79404c2b9671b87a84b5075
Brady Miller <brady.g.miller@gmail.com>
2019-01-22 23:17:37 -0800

    set up demo

--- docker/scripts/restartFarm.sh
+++ docker/scripts/restartFarm.sh
@@ -65,5 +65,5 @@ bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh ten
 bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh eleven
 bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh four
 #bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh edu empty
-bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh edu a
+#bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh edu a
 bash ~/demo_farm_openemr/docker/scripts/restartDemo.sh edu b
